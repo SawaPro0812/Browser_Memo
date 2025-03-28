baede1ab42d5b587d8b7b7c10c526fcd14c79ffc
osawa_mac <sawa.bb.pro@gmail.com>
2025-03-28 22:24:15 +0900

    dev1 関数を作成 #1

--- src/main.go
+++ src/main.go
@@ -3,11 +3,19 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+    str := returnStr()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pongss",
+			"str": str,
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func returnStr() string {
+    str := "テストです"
+    return str
 }
